747fb10846671001f7641b5f0076b3c8e96c9ed6
Alexandra Swart <answart@sbcglobal.net>
2018-02-03 11:12:13 -0800

    Add timestamp api

--- app/api/timestamp.js
+++ app/api/timestamp.js
@@ -1,11 +1,33 @@
 'use strict';
 
+var moment = require('moment');
+
+
+function formatUnix(date) {
+  return moment(date, "MMMM D, YYYY").format("X");
+}
+
+function formatNatural(unix) {
+  return moment.unix(unix).format("MMMM D, YYYY");
+}
+
 
 module.exports = function(query, cb) {
   var data = {
     natural: null,
     unix: null
   };
+  var unix;
+
+  if (Number(query) >= 0) {
+    unix = Number(query);
+  } else if (isNaN(+query) && moment(query, "MMMM D, YYYY").isValid()) {
+    unix = Number(formatUnix(query));
+  } else {
+    return cb(new Error(`Invalid query: ${query}`), data);
+  }
+  data.natural = formatNatural(unix);
+  data.unix = unix;
 
   return cb(null, data);
 };
